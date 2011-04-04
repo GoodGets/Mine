d39154c07a0edae05ce9cd838034ae7d59ca7eb0
Good Gets <goodgets@gmail.com>
2011-04-03 22:53:48 -0700

    base Rails app, no Prototype, no Test::Unit, and added .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mine::Application.config.secret_token = 'aee5b7a3efa0d2c3991986d75780dc0b5b07f19f65f9fb110efaf29efc9967e757b6c4e75eee645bc08ca71698519c878a2e5bd25609d9345aca2df4b5a7645a'
